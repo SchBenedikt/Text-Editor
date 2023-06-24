96922e011a03972c693397efb3ff43ebc187a5e6
Benedikt Schächner <137323528+SchBenedikt@users.noreply.github.com>
2023-06-24 20:26:56 +0200

    New version available

--- text-editor.py
+++ text-editor.py
@@ -1,7 +1,9 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 from docx import Document
 from docx.shared import Pt
+import os
 
 class TextEditor(QMainWindow):
     def __init__(self):
@@ -9,68 +11,90 @@ class TextEditor(QMainWindow):
         self.setWindowTitle("Text Editor")
         self.setGeometry(100, 100, 800, 600)
 
-        self.textarea = QTextEdit(self)
-        self.textarea.setFont(QFont("TkDefaultFont", 11))
-        self.setCentralWidget(self.textarea)
+        self.tab_widget = QTabWidget(self)
+        self.setCentralWidget(self.tab_widget)
+
+        self.tab_widget.setTabsClosable(True)
+        self.tab_widget.tabCloseRequested.connect(self.close_tab)
 
         self.init_menu()
         self.init_toolbar()
+        self.init_tab_bar()
+        # QApplication.setStyle(QStyleFactory.create("Fusion"))
+
+        # Set additional Fusion style options for Windows 10/11-like appearance
+        self.set_style_options()
+        self.open_new_tab()
 
     def init_menu(self):
         menubar = self.menuBar()
 
         file_menu = menubar.addMenu("File")
+
         open_action = QAction("Open", self)
         open_action.triggered.connect(self.open_file)
         file_menu.addAction(open_action)
 
-        save_action = QAction("Save", self)
-        save_action.triggered.connect(self.save_file)
-        file_menu.addAction(save_action)
-
-        export_action = QAction("Export as DOCX", self)
-        export_action.triggered.connect(self.export_as_docx)
-        file_menu.addAction(export_action)
+        new_tab_action = QAction("New Tab", self)
+        new_tab_action.triggered.connect(self.open_new_tab)
+        file_menu.addAction(new_tab_action)
 
         exit_action = QAction("Exit", self)
         exit_action.triggered.connect(qApp.quit)
         file_menu.addAction(exit_action)
 
+        save_menu = menubar.addMenu("Save")
+        save_action = QAction("Save", self)
+        save_action.triggered.connect(self.save_file)
+        save_menu.addAction(save_action)
+
+        export_docx_action = QAction("Export as DOCX", self)
+        export_docx_action.triggered.connect(self.export_as_docx)
+        save_menu.addAction(export_docx_action)
+
+        export_txt_action = QAction("Export as TXT", self)
+        export_txt_action.triggered.connect(self.export_as_txt)
+        save_menu.addAction(export_txt_action)
+
     def init_toolbar(self):
         toolbar = QToolBar(self)
         self.addToolBar(toolbar)
 
-        bold_button = QToolButton(self)
-        bold_button.setText("B")
-        bold_button.setCheckable(True)
-        bold_button.clicked.connect(self.bold_text)
-        bold_button.setStyleSheet("QToolButton { font-size: 20px; }")  # Increase the font size
-        toolbar.addWidget(bold_button)
+        toolbar.setIconSize(QSize(20, 20)) 
 
-        italic_button = QToolButton(self)
-        italic_button.setText("I")
-        italic_button.setCheckable(True)
-        italic_button.clicked.connect(self.italic_text)
-        italic_button.setStyleSheet("QToolButton { font-size: 20px; }")  # Increase the font size
-        toolbar.addWidget(italic_button)
+        bold_action = QAction(QIcon("bold.png"), "Bold", self)
+        bold_action.triggered.connect(self.bold_text)
+        toolbar.addAction(bold_action)
 
-        underline_button = QToolButton(self)
-        underline_button.setText("U")
-        underline_button.setCheckable(True)
-        underline_button.clicked.connect(self.underline_text)
-        underline_button.setStyleSheet("QToolButton { font-size: 20px; }")  # Increase the font size
-        toolbar.addWidget(underline_button)
+        italic_action = QAction(QIcon("italic.png"), "Italic", self)
+        italic_action.triggered.connect(self.italic_text)
+        toolbar.addAction(italic_action)
 
-        color_button = QToolButton(self)
-        color_button.setText("A")
-        color_button.clicked.connect(self.select_color)
-        color_button.setStyleSheet("QToolButton { font-size: 20px; }")  # Increase the font size
-        toolbar.addWidget(color_button)
+        underline_action = QAction(QIcon("underline.png"), "Underline", self)
+        underline_action.triggered.connect(self.underline_text)
+        toolbar.addAction(underline_action)
 
-    def select_color(self):
-        color = QColorDialog.getColor()
-        if color.isValid():
-            self.textarea.setTextColor(color)
+        increase_font_action = QAction(QIcon("increase_font.png"), "Increase Font Size", self)
+        increase_font_action.triggered.connect(self.increase_font_size)
+        toolbar.addAction(increase_font_action)
+
+        font_combobox = QFontComboBox(self)
+        font_combobox.setCurrentFont(QFont("TkDefaultFont"))
+        font_combobox.currentFontChanged.connect(self.change_font)
+        toolbar.addWidget(font_combobox)
+
+        change_color_action = QAction(QIcon("change_color.png"), "Change Text Color", self)
+        change_color_action.triggered.connect(self.change_text_color)
+        toolbar.addAction(change_color_action)
+
+
+
+    def init_tab_bar(self):
+        add_tab_button = QToolButton(self)
+        add_tab_button.setText("+")
+        add_tab_button.setStyleSheet("QToolButton { font-size: 20px; }")  
+        add_tab_button.clicked.connect(self.open_new_tab)
+        self.tab_widget.setCornerWidget(add_tab_button)
 
     def open_file(self):
         file, _ = QFileDialog.getOpenFileName(self, "Open File")
@@ -85,23 +109,40 @@ class TextEditor(QMainWindow):
                 except UnicodeDecodeError:
                     continue
             if content is not None:
-                self.textarea.setPlainText(content)
+                current_widget = self.tab_widget.currentWidget()
+                current_widget.setPlainText(content)
+                self.set_tab_title(current_widget, file) 
             else:
-                QMessageBox.warning(self, "Open File", "Unable to open the file with the supported encodings.")
+                QMessageBox.warning(self, "Open File", "Unable to open the file.")
 
     def save_file(self):
+        current_widget = self.tab_widget.currentWidget()
+        content = current_widget.toPlainText()
         file, _ = QFileDialog.getSaveFileName(self, "Save File")
         if file:
-            content = self.textarea.toPlainText()
-            with open(file, "w") as f:
-                f.write(content)
-            QMessageBox.information(self, "Save", "File saved successfully.")
+            try:
+                with open(file, "w") as f:
+                    f.write(content)
+                self.set_tab_title(current_widget, file)  # Set the tab title
+                QMessageBox.information(self, "Save File", "File saved successfully.")
+            except:
+                QMessageBox.warning(self, "Save File", "Unable to save the file.")
+
+    def set_tab_title(self, text_widget, file_path):
+    # Set the tab title to the file name
+        file_name = QFileInfo(file_path).fileName()
+        index = self.tab_widget.indexOf(text_widget)
+        self.tab_widget.setTabText(index, file_name)
+    # Store the file path in the text_widget for later use
+        setattr(text_widget, "file_path", file_path)
+
 
     def export_as_docx(self):
         file, _ = QFileDialog.getSaveFileName(self, "Export as DOCX", filter="*.docx")
         if file:
             doc = Document()
-            content = self.textarea.toPlainText()
+            current_widget = self.tab_widget.currentWidget()
+            content = current_widget.toPlainText()
             paragraph = doc.add_paragraph()
             runs = self.get_runs_with_formatting(content)
             for run_text, font_format in runs:
@@ -109,6 +150,20 @@ class TextEditor(QMainWindow):
                 self.apply_formatting(run, font_format)
             doc.save(file)
             QMessageBox.information(self, "Export as DOCX", "File exported successfully.")
+
+    def export_as_txt(self):
+        file, _ = QFileDialog.getSaveFileName(self, "Export as TXT", filter="*.txt")
+        if file:
+            current_widget = self.tab_widget.currentWidget()
+            content = current_widget.toPlainText()
+            try:
+                with open(file, "w") as f:
+                    f.write(content)
+                QMessageBox.information(self, "Export as TXT", "File exported successfully.")
+            except:
+                QMessageBox.warning(self, "Export as TXT", "Unable to export the file.")
+
+
 
     def apply_formatting(self, run, font_format):
         font = run.font
@@ -119,11 +174,9 @@ class TextEditor(QMainWindow):
         if font_format["underline"]:
             font.underline = True
         if font_format["color"]:
-            color = font_format["color"]
-            rgb_color = RGBColor(color.red(), color.green(), color.blue())
-            font.color.rgb = rgb_color
-        font.size = Pt(11)
-
+            rgb_color = QColor(font_format["color"])
+            font.color.rgb = rgb_color.rgb()
+        font.size = Pt(15)
 
     def get_runs_with_formatting(self, text):
         cursor = QTextCursor(self.textarea.document())
@@ -141,49 +194,191 @@ class TextEditor(QMainWindow):
                 "bold": char_format.font().bold(),
                 "italic": char_format.font().italic(),
                 "underline": char_format.font().underline(),
-                "color": char_format.foreground().color()
+                "color": char_format.foreground().color().name()
             }))
             start += 1
         return runs
+    
+    def closeEvent(self, event):
+        current_widget = self.tab_widget.currentWidget()
+        if self.is_unsaved_changes(current_widget):
+            reply = QMessageBox.question(self, "Unsaved Changes",
+                                     "There are unsaved changes. Do you want to save before exiting?",
+                                     QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if reply == QMessageBox.Save:
+                self.save_file()
+            elif reply == QMessageBox.Cancel:
+                event.ignore()
+                return
+        event.accept()
+
+    def is_unsaved_changes(self, text_widget):
+        content = text_widget.toPlainText()
+        return content != "" and content != self.get_file_content(text_widget)
+
+    def get_file_content(self, text_widget):
+        file_path = getattr(text_widget, "file_path", None)
+        if file_path:
+            try:
+                with open(file_path, "r") as file:
+                    content = file.read()
+                    return content
+            except FileNotFoundError:
+                pass
+        return ""
+
+
+ 
 
     def bold_text(self):
-        cursor = self.textarea.textCursor()
+        current_widget = self.tab_widget.currentWidget()
+        cursor = current_widget.textCursor()
         format = cursor.charFormat()
         font = format.font()
         font.setBold(not font.bold())
         format.setFont(font)
         cursor.mergeCharFormat(format)
-        self.textarea.setFocus()
+        current_widget.setFocus()
 
     def italic_text(self):
-        cursor = self.textarea.textCursor()
+        current_widget = self.tab_widget.currentWidget()
+        cursor = current_widget.textCursor()
         format = cursor.charFormat()
         font = format.font()
         font.setItalic(not font.italic())
         format.setFont(font)
         cursor.mergeCharFormat(format)
-        self.textarea.setFocus()
+        current_widget.setFocus()
 
     def underline_text(self):
-        cursor = self.textarea.textCursor()
+        current_widget = self.tab_widget.currentWidget()
+        cursor = current_widget.textCursor()
         format = cursor.charFormat()
         font = format.font()
         font.setUnderline(not font.underline())
         format.setFont(font)
         cursor.mergeCharFormat(format)
-        self.textarea.setFocus()
+        current_widget.setFocus()
+
+    def increase_font_size(self):
+        current_widget = self.tab_widget.currentWidget()
+        cursor = current_widget.textCursor()
+        format = cursor.charFormat()
+        font = format.font()
+        font_size = font.pointSize()
+        font_size += 1
+        font.setPointSize(font_size)
+        format.setFont(font)
+        cursor.mergeCharFormat(format)
+        current_widget.setFocus()
+
+    def change_font(self, font):
+        current_widget = self.tab_widget.currentWidget()
+        text_cursor = current_widget.textCursor()
+        format = text_cursor.charFormat()
+
+        font_name = font.family()
+        format.setFontFamily(font_name)
+        text_cursor.mergeCharFormat(format)
+        current_widget.setFocus()
 
     def change_text_color(self):
-        color = QColorDialog.getColor()
+        color = QColorDialog.getColor(parent=self)
         if color.isValid():
-            cursor = self.textarea.textCursor()
+            current_widget = self.tab_widget.currentWidget()
+            cursor = current_widget.textCursor()
             format = cursor.charFormat()
-            format.setForeground(QBrush(color))
+            format.setForeground(color)
             cursor.mergeCharFormat(format)
-            self.textarea.setFocus()
+            current_widget.setFocus()
+
+
+
+    def open_new_tab(self):
+        text_area = QTextEdit()
+        text_area.setFont(QFont("TkDefaultFont"))
+        text_area.textChanged.connect(self.update_tab_title) 
+        self.tab_widget.addTab(text_area, "Untitled")
+        self.tab_widget.setCurrentWidget(text_area)
+
+    def close_tab(self, index):
+        text_area = self.tab_widget.widget(index)
+        if self.is_unsaved_changes(text_area):
+            reply = QMessageBox.question(self, "Unsaved Changes",
+                                     "There are unsaved changes. Do you want to save before closing the tab?",
+                                     QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if reply == QMessageBox.Save:
+                self.save_file()
+            elif reply == QMessageBox.Cancel:
+                return
+        self.tab_widget.removeTab(index)
+
+    def update_tab_title(self):
+        current_widget = self.tab_widget.currentWidget()
+        current_index = self.tab_widget.currentIndex()
+        if self.is_unsaved_changes(current_widget):
+            file_path = getattr(current_widget, "file_path", None)
+            if file_path:
+                file_name = os.path.basename(file_path)
+                self.tab_widget.setTabText(current_index, file_name +" *")
+            else:
+                self.tab_widget.setTabText(current_index, "Unsaved *")
+        else:
+            file_path = getattr(current_widget, "file_path", None)
+            if file_path:
+                file_name = os.path.basename(file_path)
+                self.tab_widget.setTabText(current_index, file_name)
+            else:
+                self.tab_widget.setTabText(current_index, "Untitled")
+
+    def set_style_options(self):
+        style_sheet = """
+QTabWidget::pane {
+    background-color: #FFFFFF;
+}
+
+QTabWidget::tab-bar {
+    alignment: left;
+    height: auto;
+}
+
+QTabBar::tab {
+    background-color: #FFFFFF;
+    color: #050000;
+    border: 1px solid #C0C0C0;
+    padding: 10px 0;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    width: 100px;
+    height: 10px;
+    text-align: center; 
+    transition: background-color 0.3s ease, color 0.3s ease;
+}
+
+QTabBar::tab:selected {
+    background-color: #FFFFFF;
+    color: #000000;
+    border-bottom-color: #FFFFFF;
+}
+
+QTabBar::tab:!selected {
+    margin-top: 2px;
+    background-color: #ebfcfc;
+}
+
+QTabBar::tab:hover {
+    background-color: #a5faf8; 
+    color: #303030; 
+    cursor: pointer; 
+}
+
+
+        """
+        self.setStyleSheet(style_sheet)
+
 
 if __name__ == "__main__":
     app = QApplication([])
     text_editor = TextEditor()
     text_editor.show()
-    app.exec()
+    app.exec_()
